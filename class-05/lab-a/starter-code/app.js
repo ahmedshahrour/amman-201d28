2ad5231bcb2b2780506500574aa1911be6cefa93
ahmedshahrour <ahmed89sh@gmail.com>
2021-06-10 16:54:00 +0300

    problem2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -31,10 +31,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-let multiply = a*b ;
-let str6 ='The product of '+a+' and ' +b+ ' is '+ multiply + '.';
- return [multiply, str6]
-}
+  let multiply = a*b ;
+  let str6 ='The product of '+a+' and ' +b+ ' is '+ multiply + '.';
+   return [multiply, str6]
+  }
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
